f23c667346b58cd9d93d5ce808320e87f0a011c6
Joy <utsho233@gmail.com>
2021-06-27 02:58:15 +0600

    jsx2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+
+
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+
+
+
+
+
+
 function App() {
-  const element = <h1>Hello, world!</h1>;
+  // const element = <h1>Hello, world!</h1>;
   return (
     <div className="App">
       <h1>{element}</h1>
